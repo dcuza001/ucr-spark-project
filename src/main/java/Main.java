138314877716af21ae80d60813e2982592083d9f
pjuhel <juhel.pierre76@gmail.com>
2017-02-06 03:15:10 -0800

    First implementation of twitter4j, correctly retrieving hashtags and woeid.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,46 @@
  */
 import static spark.Spark.*;
 
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        //get("/hello", (req, res) -> "Hello World");
+
+
+        try {
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("3KzLWKJbuGHBFvqcFoePCFdgB")
+                    .setOAuthConsumerSecret("ekSz63jNGILqD1mcYxYQKdYXS8rbnJ1mMjqqc8lBAsmsMJZH22")
+                    .setOAuthAccessToken("1004991048-pXYIrr9BgTaVw4RYLV28QzJGF3SXFcScM5Qq3pP")
+                    .setOAuthAccessTokenSecret("mMLsiETvALF1ACGMqQ4RLTiEHnn9CE5ytuQsZWSt1o4Cz");
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            ResponseList<Location> locations;
+
+            System.out.println("Obtaining available trends");
+            locations = twitter.getAvailableTrends();
+            for (Location location : locations) {
+                System.out.println(location.getName() + " (woeid:" + location.getWoeid() + ")");
+            }
+
+            Trends trends = twitter.getPlaceTrends(1);
+            System.out.println("Showing trends worldwide with WOEID = 1");
+            System.out.println("As of : " + trends.getAsOf());
+            for (Trend trend : trends.getTrends()) {
+                System.out.println(" " + trend.getName());
+            }
+            System.out.println("done.");
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get trends: " + te.getMessage());
+            System.exit(-1);
+        }
     }
 }
